893a773f26e920cf2a61b2efc8fd6ef6378643e4
Gleidson Lima <gleidsonlimacrf@gmail.com>
2024-09-24 23:01:51 -0300

    Nova home finalizada

--- login.js
+++ login.js
@@ -1,3 +1,12 @@
 //Trabalhando no login
 
 //finalizando sistema de login
+
+function login (nome) {
+    return nome;
+}
+
+//Seja bem vindo
+
+//Sistema de login completo
+//Retornando o nome do usuario quando logado 
